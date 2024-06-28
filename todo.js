309ec94b3676d32936e42a76cc4a4a693d01dd91
BekirLee <nihadbekirli8@gmail.com>
2024-06-29 00:05:07 +0400

    add new codes

--- todo.js
+++ todo.js
@@ -1,12 +1,39 @@
 const form = document.getElementById("todo-form");
 const input = document.getElementById("todo-input");
-const todoLane = document.getElementById("doing-lane");
+
+let todo = document.getElementById("todo");
+let doing = document.getElementById("doing");
+let done = document.getElementById("done");
+
 const addTask = document.querySelector('.add-task');
+const addTasks = document.querySelector('.add-tasks');
 const menu = document.querySelector('.drop-down--menu');
+const menus = document.querySelector('.menu');
+
+const btns = document.querySelectorAll('.btn');
+let selectedCol = todo;
+
+document.querySelector('.btn-todo').addEventListener('click', () => {
+  selectedCol = todo;
+});
+
+document.querySelector('.btn-doing').addEventListener('click', () => {
+  selectedCol = doing;
+});
+
+document.querySelector('.btn-done').addEventListener('click', () => {
+  selectedCol = done;
+});
 
 addTask.addEventListener('click', () => {
   menu.classList.toggle('disabled')
 })
+
+addTasks.addEventListener('click', () => {
+  menus.classList.toggle('disabled')
+})
+
+// console.log(todoLane)
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -27,7 +54,21 @@ form.addEventListener("submit", (e) => {
     newTask.classList.remove("is-dragging");
   });
 
-  todoLane.appendChild(newTask);
+  selectedCol.appendChild(newTask);
+
+
+  // btns.forEach((hell) => {
+  //   let btnContent = hell.textContent
+  //   if (btnContent == todo.textContent) {
+  //     todo.appenChild(newTask)
+  //   }
+  //   else if (btnContent == doing.textContent) {
+  //     doing.appendChild(newTask)
+  //   }
+  //   else {
+  //     done.appendChild(newTask);
+  //   }
+  // })
 
   input.value = "";
 });
