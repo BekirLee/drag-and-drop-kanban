c2fed75f7f5549ec65b38e092fa27ca9ee28c080
BekirLee <nihadbekirli8@gmail.com>
2024-07-09 18:18:45 +0400

    new added things after a long time

--- todo.js
+++ todo.js
@@ -1,3 +1,220 @@
+// const mainBtn = document.querySelector('.main-task--buton');
+// const mainTaskBox = document.querySelector('.tasks');
+// const board = document.querySelector('.board');
+
+// let isClicked = false;
+
+// mainBtn.addEventListener('click', (e) => {
+//   mainTaskBox.classList.toggle('disabled');
+//   board.classList.toggle('background-overlay');
+
+//   if (isClicked) {
+//     e.target.innerHTML = 'Add Task';
+//   } else {
+//     e.target.innerHTML = 'Quit';
+//   }
+//   isClicked = !isClicked;
+// });
+
+// fetchTasks();
+
+// async function addTask() {
+//   const title = document.getElementById('new-task--title').value;
+//   const image = document.getElementById('new-task--image').files[0];
+//   const status = document.getElementById('new-task--status').value;
+
+//   // const titleİnput = document.querySelector('.new-task--title');
+
+//   if (!title) {
+//     alert("Task need!");
+//     return;
+//   }
+
+//   const task = {
+//     title: title,
+//     status: status,
+//     image: null
+//   };
+
+//   if (image) {
+//     const reader = new FileReader();
+//     reader.onload = async function (event) {
+//       task.image = event.target.result;
+//       const savedTask = await sendTaskToServer(task);
+//       addTaskToBoard(savedTask);
+//     }
+//     reader.readAsDataURL(image);
+//   } else {
+//     const savedTask = await sendTaskToServer(task);
+//     addTaskToBoard(savedTask);
+//   }
+
+//   document.getElementById('new-task--title').value = '';
+//   document.getElementById('new-task--image').value = '';
+//   document.getElementById('new-task--status').value = 'todo';
+// }
+
+// async function sendTaskToServer(task) {
+//   try {
+//     const response = await fetch('http://localhost:3000/progresses', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json'
+//       },
+//       body: JSON.stringify(task)
+//     });
+//     if (!response.ok) {
+//       throw new Error('Error');
+//     }
+//     return await response.json();
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+// function addTaskToBoard(task) {
+//   const taskDiv = document.createElement('div');
+//   taskDiv.classList.add('task');
+//   taskDiv.setAttribute('data-id', task.id);
+//   taskDiv.setAttribute('draggable', true);
+//   taskDiv.innerHTML = `
+//     <h4>${task.title}</h4>
+//     ${task.image ? `<img class='task-photo' src="${task.image}" />` : '---'}
+//     <button onclick="deleteTask('${task.id}')">Delete</button>
+//   `;
+//   console.log(task.status)
+//   document.getElementById(task.status).appendChild(taskDiv);
+
+//   dragStart();
+// }
+
+// async function fetchTasks() {
+//   try {
+//     const response = await fetch('http://localhost:3000/progresses');
+//     if (!response.ok) {
+//       throw new Error('error');
+//     }
+//     const tasks = await response.json();
+//     tasks.forEach(task => addTaskToBoard(task)),
+//       console.log(tasks)
+//   } catch (error) {
+//     console.error('error var', error);
+//   }
+// }
+
+// async function deleteTask(id) {
+//   try {
+//     const response = await fetch(`http://localhost:3000/progresses/${id}`, {
+//       method: 'DELETE'
+//     });
+//     if (!response.ok) {
+//       throw new Error('Görev silinirken hata oluştu');
+//     }
+//     const taskDiv = document.querySelector(`.task[data-id='${id}']`);
+//     if (taskDiv) {
+//       taskDiv.remove();
+//     }
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+// // 
+// document.getElementById('add-column-btn').addEventListener('click', addColumn);
+
+// function addColumn() {
+//   const columnName = prompt("Enter column name:");
+//   if (!columnName) return;
+
+//   const columnId = columnName.toLowerCase().replace(/\s+/g, '-');
+//   const column = document.createElement('div');
+//   column.classList.add('column');
+//   column.innerHTML = `<h2>${columnName}</h2>`;
+//   column.setAttribute('id', columnId);
+
+//   document.getElementById('board').appendChild(column);
+//   enableDragAndDrop(column);
+//   // console.log(enableDragAndDrop(column));
+
+//   addColumnToSelect(columnName, columnId);
+// }
+
+// function addColumnToSelect(columnName, columnId) {
+//   const select = document.getElementById('new-task--status');
+//   const option = document.createElement('option');
+//   option.value = columnId;
+//   option.text = columnName;
+//   select.appendChild(option);
+// }
+
+// async function enableDragAndDrop(column) {
+//   column.addEventListener('dragover', (e) => {
+//     e.preventDefault();
+//     const mouseY = e.clientY;
+//     const bottomTask = insertTaskAtPosition(column, mouseY);
+//     const curTask = document.querySelector('.is-dragging');
+//     if (!bottomTask) {
+//       column.appendChild(curTask);
+//     } else {
+//       column.insertBefore(curTask, bottomTask);
+//     }
+//   });
+//   column.addEventListener('dragstart', () => {
+//     console.log('hello')
+//   })
+//   column.addEventListener('dragend', () => {
+//     console.log('end')
+//   })
+
+//   column.addEventListener('drop', (e) => {
+//     e.preventDefault();
+//   });
+// }
+
+// // async function saveColumnToServer(column) {
+// //   try {
+// //     const response = await fetch('http://localhost:3000/columns', {
+// //       method: 'POST',
+// //       headers: {
+// //         'Content-Type': 'application/json'
+// //       },
+// //       body: JSON.stringify(column)
+// //     });
+// //     if (!response.ok) {
+// //       throw new Error('Error saving column');
+// //     }
+// //   } catch (error) {
+// //     console.error(error);
+// //   }
+// // }
+
+// // 
+// // Function to insert task at the correct position based on mouse position
+// function insertTaskAtPosition(zone, mouseY) {
+//   const tasks = zone.querySelectorAll('.task:not(.is-dragging)');
+//   let closestTask = null;
+//   let closestOffset = Number.POSITIVE_INFINITY;
+
+//   tasks.forEach(task => {
+//     const { top, bottom } = task.getBoundingClientRect();
+//     const offsetTop = mouseY - top;
+//     const offsetBottom = bottom - mouseY;
+
+//     if (offsetTop > 0 && offsetTop < closestOffset) {
+//       closestTask = task.nextElementSibling;;
+//       closestOffset = offsetTop;
+//     }
+
+//     if (offsetBottom > 0 && offsetBottom < closestOffset) {
+//       closestTask = task;
+//       closestOffset = offsetBottom;
+//     }
+//   });
+
+//   return closestTask;
+// }
+
+
 const mainBtn = document.querySelector('.main-task--buton');
 const mainTaskBox = document.querySelector('.tasks');
 const board = document.querySelector('.board');
@@ -16,17 +233,20 @@ mainBtn.addEventListener('click', (e) => {
   isClicked = !isClicked;
 });
 
-fetchTasks();
+fetchColumnsAndTasks();
+
+async function fetchColumnsAndTasks() {
+  await fetchColumns();
+  await fetchTasks();
+}
 
 async function addTask() {
   const title = document.getElementById('new-task--title').value;
   const image = document.getElementById('new-task--image').files[0];
   const status = document.getElementById('new-task--status').value;
 
-  // const titleİnput = document.querySelector('.new-task--title');
-
   if (!title) {
-    alert("Task need!");
+    alert("Task title is required!");
     return;
   }
 
@@ -42,7 +262,7 @@ async function addTask() {
       task.image = event.target.result;
       const savedTask = await sendTaskToServer(task);
       addTaskToBoard(savedTask);
-    }
+    };
     reader.readAsDataURL(image);
   } else {
     const savedTask = await sendTaskToServer(task);
@@ -64,7 +284,7 @@ async function sendTaskToServer(task) {
       body: JSON.stringify(task)
     });
     if (!response.ok) {
-      throw new Error('Error');
+      throw new Error('Error saving task');
     }
     return await response.json();
   } catch (error) {
@@ -91,13 +311,12 @@ async function fetchTasks() {
   try {
     const response = await fetch('http://localhost:3000/progresses');
     if (!response.ok) {
-      throw new Error('error');
+      throw new Error('Error fetching tasks');
     }
     const tasks = await response.json();
-    tasks.forEach(task => addTaskToBoard(task)),
-      console.log(tasks)
+    tasks.forEach(task => addTaskToBoard(task));
   } catch (error) {
-    console.error('error var', error);
+    console.error('Error fetching tasks', error);
   }
 }
 
@@ -107,7 +326,7 @@ async function deleteTask(id) {
       method: 'DELETE'
     });
     if (!response.ok) {
-      throw new Error('Görev silinirken hata oluştu');
+      throw new Error('Error deleting task');
     }
     const taskDiv = document.querySelector(`.task[data-id='${id}']`);
     if (taskDiv) {
@@ -118,24 +337,65 @@ async function deleteTask(id) {
   }
 }
 
-// 
 document.getElementById('add-column-btn').addEventListener('click', addColumn);
 
-function addColumn() {
+async function addColumn() {
   const columnName = prompt("Enter column name:");
   if (!columnName) return;
 
-  const columnId = columnName.toLowerCase().replace(/\s+/g, '-');
-  const column = document.createElement('div');
-  column.classList.add('column');
-  column.innerHTML = `<h2>${columnName}</h2>`;
-  column.setAttribute('id', columnId);
+  // const columnId = columnName.toLowerCase().replace(/\s+/g, '-');
+  const column = {
+    // title: 'columnName',
+    status: columnName,
+    // image: null
+  };
 
-  document.getElementById('board').appendChild(column);
-  enableDragAndDrop(column);
-  // console.log(enableDragAndDrop(column));
+  const savedColumn = await saveColumnToServer(column);
+  addColumnToBoard(savedColumn);
+  addColumnToSelect(savedColumn.title, savedColumn.id);
+}
 
-  addColumnToSelect(columnName, columnId);
+async function saveColumnToServer(column) {
+  try {
+    const response = await fetch('http://localhost:3000/progresses', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(column)
+    });
+    if (!response.ok) {
+      throw new Error('Error saving column');
+    }
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function fetchColumns() {
+  try {
+    const response = await fetch('http://localhost:3000/progresses');
+    if (!response.ok) {
+      throw new Error('Error fetching columns');
+    }
+    const columns = await response.json();
+    columns.forEach(column => {
+      // addColumnToBoard(column);
+      addColumnToSelect(column.title, column.id);
+    });
+  } catch (error) {
+    console.error('Error fetching columns', error);
+  }
+}
+
+function addColumnToBoard(column) {
+  const columnDiv = document.createElement('div');
+  columnDiv.classList.add('column');
+  // columnDiv.setAttribute('id', column.id);
+  columnDiv.innerHTML = `<h2>${column.status}</h2>`;
+  document.getElementById('board').appendChild(columnDiv);
+  enableDragAndDrop(columnDiv);
 }
 
 function addColumnToSelect(columnName, columnId) {
@@ -158,20 +418,12 @@ async function enableDragAndDrop(column) {
       column.insertBefore(curTask, bottomTask);
     }
   });
-  column.addEventListener('dragstart', () => {
-    console.log('hello')
-  })
-  column.addEventListener('dragend', () => {
-    console.log('end')
-  })
 
   column.addEventListener('drop', (e) => {
     e.preventDefault();
   });
 }
 
-// 
-// Function to insert task at the correct position based on mouse position
 function insertTaskAtPosition(zone, mouseY) {
   const tasks = zone.querySelectorAll('.task:not(.is-dragging)');
   let closestTask = null;
@@ -183,7 +435,7 @@ function insertTaskAtPosition(zone, mouseY) {
     const offsetBottom = bottom - mouseY;
 
     if (offsetTop > 0 && offsetTop < closestOffset) {
-      closestTask = task.nextElementSibling;;
+      closestTask = task.nextElementSibling;
       closestOffset = offsetTop;
     }
 
